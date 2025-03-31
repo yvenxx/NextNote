60d9030fea8d93b4ee195d412509161b2cb7ac0a
yven <yvenxx@163.com>
2025-03-31 14:52:45 +0800

    feat(about):页面添加

---
+++ nextnote-admin-ui/src/views/home/about.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home-container">
+    <app-header />
+    <el-row class="main-container" :gutter="30" type="flex" justify="left">
+      <el-col :offset="4" :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+        <h1>关于</h1>
+        啥也没有
+      </el-col>
+    </el-row>
+    <app-footer />
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/Header.vue";
+import AppFooter from "@/components/Footer.vue";
+export default {
+  name: "About",
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+  data() {
+    return {
+      about: {},
+    };
+  },
+  created() {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+.home-container {
+  min-height: 100vh;
+  background-color: #fff;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif;
+}
+.main-container {
+  width: 100%;
+  margin: 0 auto;
+  margin-top: 20px;
+  padding: 2rem 0;
+}
+</style>
